939e404d42bda25be57168101df7e89748a7b3db
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-20 22:22:35 +0200

    Lesson 2

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -7,7 +7,7 @@ suite("Unit Tests", function () {
     test("#isNull, #isNotNull", function () {
       assert.isNull(
         null,
-        "This is an optional error description - e.g. null is null"
+        "this is an optional error description - e.g. null is null"
       );
       assert.isNotNull(1, "1 is not null");
     });
